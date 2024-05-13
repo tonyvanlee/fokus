49b8e661b090cb2479ac5ae87283d29c9ca5df55
Antonio Vanderli dos Santos <tony.datascientist01@gmail.com>
2024-05-13 10:13:51 -0300

    projeto fokus

---
+++ exercicios.js
@@ -0,0 +1,6 @@
+const html = document.querySelector('html')
+const timer = document.querySelector('#timer')
+const primary_button = document.querySelector('.app__card-primary-button')
+const card_list = document.querySelector('.app__card-list-item')
+const title = document.querySelector('.app__title')
+
